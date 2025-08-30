cb0c8167469f23814595f78591321f06e9879b28
Abhishek. AN <abhishekashok641@gmail.com>
2025-08-30 23:10:02 +0530

    Add files via upload
    add all left over files

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './index.css';
+import Navigation from './components/navigation';
+import HeroSection from './components/hero';
+function App() {
+  return (
+    <div>   
+    <Navigation  />
+    <HeroSection />
+   </div>
+
+  );
+}
+
+export default App;
